37412a5d3d9480b1e870f1f4114276e2e6558928
MarinaB77777 <ramotaugu@gmail.com>
2026-07-22 17:48:43 -0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,7 +2,10 @@ import uuid
 import json
 import shutil
 import os
-from ai_circuit.gemini_service import ask_gemini_free
+try:
+    from ai_circuit.gemini_service import ask_gemini_free
+except ModuleNotFoundError:
+    from api.ai_circuit.gemini_service import ask_gemini_free
 from copy import deepcopy
 from collections import defaultdict
 from datetime import UTC, datetime
